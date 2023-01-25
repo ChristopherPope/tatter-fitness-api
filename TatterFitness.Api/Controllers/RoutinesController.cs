a4c46bada349266e305e129398a77c69cc4ec08a
Christopher Pope <christopher@thepopes.org>
2023-01-24 19:28:27 -0500

    .

---
+++ TatterFitness.Api/Controllers/RoutinesController.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using TatterFitness.Bll.Exceptions;
+using TatterFitness.Bll.Interfaces.Services;
+using TatterFitness.Models;
+
+namespace TatterFitness.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoutinesController : BaseController
+    {
+        private const string RoutineDoesNotExistDescription = "The Routine does not exist.";
+        private const string UserNotAuthorizedDescription = "The authtenticated user does not have access to the Routine.";
+        private readonly IRoutinesService routineService;
+
+        public RoutinesController(ILogger<RoutinesController> logger, IRoutinesService routineService)
+            : base(logger)
+        {
+            this.routineService = routineService;
+        }
+
+        [HttpGet]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, BadRequestDescription)]
+        [SwaggerOperation("Return the Routines for the current authorized user.")]
+        public ActionResult<IEnumerable<Routine>> ReadRoutines()
+        {
+            try
+            {
+                LogApiCall();
+                var routines = routineService.ReadRoutines();
+                LogApiResult(routines);
+
+                return routines.ToList();
+            }
+            catch (InvalidOperationException ex)
+            {
+                logger.LogError(ex, "Unable to read routines.");
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [SwaggerResponse(StatusCodes.Status201Created, "The new Routine has been created.")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, BadRequestDescription)]
+        [SwaggerOperation("Create a new Routine.", "The new Routine will be associated with the authetnicated user.")]
+        public ActionResult<Routine> CreateRoutine([FromBody] Routine routine)
+        {
+            try
+            {
+                LogApiCall();
+                routine = routineService.CreateRoutine(routine);
+                LogApiResult(routine);
+
+                return Created($"Routines/{routine.Id}", routine);
+            }
+            catch (InvalidOperationException ex)
+            {
+                logger.LogError(ex, "Unable to create a new Routine.");
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [SwaggerResponse(200, "The Routine with the given Routine ID is returned.")]
+        [SwaggerResponse(404, RoutineDoesNotExistDescription)]
+        [SwaggerResponse(401, UserNotAuthorizedDescription)]
+        [SwaggerResponse(400, BadRequestDescription)]
+        [SwaggerOperation("Return a Routine", "The Routine Id must be owned by the authenticated user.")]
+        [Route("{routineId}")]
+        public ActionResult<Routine> ReadRoutine(int routineId)
+        {
+            try
+            {
+                LogApiCall();
+
+                var routine = routineService.ReadRoutine(routineId);
+                LogApiResult(routine);
+
+                return routine;
+            }
+            catch (UserNotAuthorizedException ex)
+            {
+                logger.LogError(ex, "Unable to read routine {routineId}.", routineId);
+                return Unauthorized(ex.Message);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                logger.LogError(ex, "Unable to read routine {routineId}.", routineId);
+                return NotFound(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                logger.LogError(ex, "Unable to read routine {routineId}.", routineId);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPatch]
+        [SwaggerResponse(204, "The update to the Routine was successfull.")]
+        [SwaggerResponse(404, RoutineDoesNotExistDescription)]
+        [SwaggerResponse(401, UserNotAuthorizedDescription)]
+        [SwaggerResponse(400, BadRequestDescription)]
+        [SwaggerOperation("Update a Routine", "The Routine Id must be owned by the authenticated user.")]
+        [Route("{routineId}")]
+        public IActionResult UpdateRoutine(int routineId, [FromBody] JsonPatchDocument patchDoc)
+        {
+            try
+            {
+                LogApiCall();
+                var routine = routineService.ReadRoutine(routineId);
+                patchDoc.ApplyTo(routine);
+                routine.Id = routineId;
+                routineService.UpdateRoutine(routine);
+
+                return NoContent();
+            }
+            catch (UserNotAuthorizedException ex)
+            {
+                logger.LogError(ex, "Unable to update routine {routineId}.", routineId);
+                return Unauthorized(ex.Message);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                logger.LogError(ex, "Unable to update routine {routineId}.", routineId);
+                return NotFound(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                logger.LogError(ex, "Unable to update routine {routineId}.", routineId);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [SwaggerResponse(204, "The Routine was deleted successfully.")]
+        [SwaggerResponse(404, RoutineDoesNotExistDescription)]
+        [SwaggerResponse(401, UserNotAuthorizedDescription)]
+        [SwaggerResponse(400, BadRequestDescription)]
+        [SwaggerOperation("Delete a Routine", "The Routine Id must be owned by the authenticated user.")]
+        [Route("{routineId}")]
+        public IActionResult DeleteRoutine(int routineId)
+        {
+            try
+            {
+                LogApiCall();
+
+                routineService.DeleteRoutine(routineId);
+            }
+            catch (UserNotAuthorizedException ex)
+            {
+                logger.LogError(ex, "Unable to delete routine {routineId}.", routineId);
+                return Unauthorized(ex.Message);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                logger.LogError(ex, "Unable to delete routine {routineId}.", routineId);
+                return NotFound(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                logger.LogError(ex, "Unable to delete routine {routineId}.", routineId);
+                return BadRequest(ex.Message);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ TatterFitness.Api/Controllers/WorkoutExerciesController.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using TatterFitness.Bll.Exceptions;
+using TatterFitness.Bll.Interfaces.Services;
+using TatterFitness.Models.Workouts;
+
+namespace TatterFitness.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkoutExercisesController : BaseController
+    {
+        private const string UserNotAuthorizedDescription = "The authenticated user does not have access to the Workout.";
+        private const string WorkoutDoesNotExistDescription = "The Workout or the Exercise does not exist.";
+        private const string WorkoutExerciseDoesNotExistDescription = "The WorkoutExercise does not exist.";
+        private readonly IWorkoutExercisesService exercisesSvc;
+
+        public WorkoutExercisesController(ILogger<WorkoutExercisesController> logger, IWorkoutExercisesService exercisesSvc)
+            : base(logger)
+        {
+            this.exercisesSvc = exercisesSvc;
+        }
+
+        [HttpPatch]
+        [SwaggerResponse(204, "The update to the WorkoutExercise was successfull.")]
+        [SwaggerResponse(404, WorkoutExerciseDoesNotExistDescription)]
+        [SwaggerResponse(401, UserNotAuthorizedDescription)]
+        [SwaggerResponse(400, BadRequestDescription)]
+        [SwaggerOperation("Update a WorkoutExercise", "The WorkoutExercise must be owned by the authenticated user.")]
+        [Route("{workoutExerciseId}")]
+        public IActionResult Update(int workoutExerciseId, [FromBody] JsonPatchDocument patchDoc)
+        {
+            try
+            {
+                LogApiCall();
+                var workoutExercise = exercisesSvc.Read(workoutExerciseId);
+                patchDoc.ApplyTo(workoutExercise);
+                workoutExercise.Id = workoutExerciseId;
+                exercisesSvc.Update(workoutExercise);
+
+                return NoContent();
+            }
+            catch (UserNotAuthorizedException ex)
+            {
+                logger.LogError(ex, "Unable to update workout exercise {workoutExerciseId}.", workoutExerciseId);
+                return Unauthorized(ex.Message);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                logger.LogError(ex, "Unable to update workout exercise {workoutExerciseId}.", workoutExerciseId);
+                return NotFound(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                logger.LogError(ex, "Unable to update workout exercise {workoutExerciseId}.", workoutExerciseId);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [SwaggerResponse(StatusCodes.Status200OK, "Returns the most recent WorkoutExercises for the given list of ExerciseIds.")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "An Exercise does not exist.")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, BadRequestDescription)]
+        [SwaggerOperation("Returns the most recent WorkoutExercise for a given list ExerciseIds.")]
+        [Route("MostRecent")]
+        public ActionResult<IEnumerable<WorkoutExercise>> ReadMostRecent([FromQuery] IEnumerable<int> exerciseIds)
+        {
+            try
+            {
+                LogApiCall();
+                var workoutExercises = exercisesSvc.ReadMostRecent(exerciseIds).ToList();
+                LogApiResult(workoutExercises);
+
+                return workoutExercises;
+            }
+            catch (EntityNotFoundException ex)
+            {
+                logger.LogError(ex, "Unable to read most recent exercises");
+                return NotFound(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                logger.LogError(ex, "Unable to read most recent exercises");
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [SwaggerResponse(StatusCodes.Status201Created, "Returns the newly created WorkoutExercise.")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, WorkoutDoesNotExistDescription)]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, UserNotAuthorizedDescription)]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, BadRequestDescription)]
+        [SwaggerOperation("Create a new WorkoutExercise.", "The Workout must belong to the authenticated user.")]
+        public ActionResult<WorkoutExercise> Create(WorkoutExercise we)
+        {
+            try
+            {
+                LogApiCall();
+                we = exercisesSvc.Create(we);
+                LogApiResult(we);
+
+                return Created($"WorkoutExercises/{we.Id}", we);
+            }
+            catch (UserNotAuthorizedException ex)
+            {
+                logger.LogError(ex, "Unable to read most create exercise.");
+                return Unauthorized(ex.Message);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                logger.LogError(ex, "Unable to read most create exercise.");
+                return NotFound(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                logger.LogError(ex, "Unable to read most create exercise.");
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [SwaggerResponse(StatusCodes.Status204NoContent, "The WorkoutExercise has been deleted.")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Either the Workout does not exist or the Exercise does not exist on the Workout.")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, UserNotAuthorizedDescription)]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, BadRequestDescription)]
+        [SwaggerOperation("Delete a WorkoutExercise.", "The Workout must belong to the authenticated user.")]
+        [Route("{workoutExerciseId}")]
+        public ActionResult RemoveExerciseFromWorkout(int workoutExerciseId)
+        {
+            try
+            {
+                LogApiCall();
+                exercisesSvc.Delete(workoutExerciseId);
+                return NoContent();
+            }
+            catch (UserNotAuthorizedException ex)
+            {
+                logger.LogError(ex, "Unable to read most remove exercise from workout {workoutExerciseId}.", workoutExerciseId);
+                return Unauthorized(ex.Message);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                logger.LogError(ex, "Unable to read most remove exercise from workout {workoutExerciseId}.", workoutExerciseId);
+                return NotFound(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                logger.LogError(ex, "Unable to read most remove exercise from workout {workoutExerciseId}.", workoutExerciseId);
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
